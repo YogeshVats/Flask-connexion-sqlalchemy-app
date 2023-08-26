13a4286c101458db6bb898d25cf8e13768890682
Yogesh <yogesh.vatz@gmail.com>
2023-08-26 19:12:11 +0530

    Addcode for second part of the series differentiate the first and second part files

--- app_first.py
+++ app_first.py
@@ -6,12 +6,12 @@ import connexion
 
 
 app = connexion.App(__name__, specification_dir="./")
-app.add_api("swagger.yml")
+app.add_api("swagger_first.yml")
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home_first.html")
 
 
 if __name__ == '__main__':
