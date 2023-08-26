1076648c265793e44a5668eac1f6ca952094d2ed
Yogesh <yogesh.vatz@gmail.com>
2023-08-26 17:58:19 +0530

    Initial commit Add sqlAlchemy model and marshmallow model. Before the peoples static data cleanup

---
+++ app.py
@@ -0,0 +1,19 @@
+# app.py this is the main application
+
+from flask import render_template, abort # Remove: import Flask
+
+import connexion
+
+
+app = connexion.App(__name__, specification_dir="./")
+app.add_api("swagger.yml")
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)      # Flask’s development server defaults to host 127.0.0.1 
+                                                        # and port 5000
